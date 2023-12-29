cbcdcfba3d864053a402d32647e8ed2c86600f6a
m4chado <enzo653fm@gmail.com>
2023-12-29 16:01:48 -0300

    refactor: change error message

--- api/src/shared/domain/errors/validation-error.ts
+++ api/src/shared/domain/errors/validation-error.ts
@@ -4,7 +4,7 @@ export class ValidationError extends Error {}
 
 export class EntityValidationError extends Error {
   constructor(public error: FieldsError) {
-    super('Entity Validation Error');
+    super('Entity validation error');
     this.name = 'EntityValidationError';
   }
 }
