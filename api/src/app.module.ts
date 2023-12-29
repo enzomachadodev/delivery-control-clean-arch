ce794316f11d8e94679f0f3c4f7ebe967659379d
m4chado <enzo653fm@gmail.com>
2023-12-29 10:03:11 -0300

    feat: create users resouces

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
+import { EnvConfigModule } from './shared/infra/env-config/env-config.module';
+import { UsersModule } from './users/infra/users.module';
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
