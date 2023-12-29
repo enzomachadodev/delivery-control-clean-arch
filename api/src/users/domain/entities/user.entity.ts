89d689e1681d94143742f1457d681bc2196cd081
m4chado <enzo653fm@gmail.com>
2023-12-29 10:24:50 -0300

    feat: create getter methods of UserEntity

--- api/src/users/domain/entities/user.entity.ts
+++ api/src/users/domain/entities/user.entity.ts
@@ -11,4 +11,24 @@ export class UserEntity {
     this.props.createdAt = this.props.createdAt ?? new Date();
     this.props.updatedAt = this.props.updatedAt ?? new Date();
   }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
 }
