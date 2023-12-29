dc5c0183bea67b250f524aac1b9576765974a8e0
m4chado <enzo653fm@gmail.com>
2023-12-29 10:17:57 -0300

    refactor: change createdAt and updatedAt to optional

--- api/src/users/domain/entities/user.entity.ts
+++ api/src/users/domain/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class UserEntity {}
+export type UserProps = {
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date();
+    this.props.updatedAt = this.props.updatedAt ?? new Date();
+  }
+}
