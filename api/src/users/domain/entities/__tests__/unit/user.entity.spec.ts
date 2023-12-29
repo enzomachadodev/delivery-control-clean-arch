9ef9c9a429f9fb845b8dd2f760acf4bc1f43162d
m4chado <enzo653fm@gmail.com>
2023-12-29 10:25:09 -0300

    test: create getter methods unit tests

--- api/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ api/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -19,4 +19,32 @@ describe('UserEntity unit tests', () => {
     expect(sut.props.createdAt).toEqual(expect.any(Date));
     expect(sut.props.updatedAt).toEqual(expect.any(Date));
   });
+
+  it('Getter of name field', () => {
+    expect(sut.name).toBeDefined();
+    expect(sut.name).toEqual(props.name);
+    expect(typeof sut.name).toBe('string');
+  });
+
+  it('Getter of email field', () => {
+    expect(sut.email).toBeDefined();
+    expect(sut.email).toEqual(props.email);
+    expect(typeof sut.email).toBe('string');
+  });
+
+  it('Getter of password field', () => {
+    expect(sut.password).toBeDefined();
+    expect(sut.password).toEqual(props.password);
+    expect(typeof sut.password).toBe('string');
+  });
+
+  it('Getter of createdAt field', () => {
+    expect(sut.createdAt).toBeDefined();
+    expect(sut.createdAt).toBeInstanceOf(Date);
+  });
+
+  it('Getter of updatedAt field', () => {
+    expect(sut.updatedAt).toBeDefined();
+    expect(sut.updatedAt).toBeInstanceOf(Date);
+  });
 });
