57619d7d875583e91bdf010bdefea9d5af45b351
m4chado <enzo653fm@gmail.com>
2023-12-28 23:27:50 -0300

    feat: implement EnvConfig interface

--- api/src/shared/infrastructure/env-config/env-config.service.ts
+++ api/src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private configService: ConfigService) {}
+
+  getAppPort(): number {
+    return Number(this.configService.get('PORT'));
+  }
+
+  getNodeEnv(): string {
+    return this.configService.get('NODE_ENV');
+  }
+}
