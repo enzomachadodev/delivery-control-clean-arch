d53014c42b171763513f8046fcf8b4749bafa1eb
m4chado <enzo653fm@gmail.com>
2023-12-29 23:32:29 -0300

    feat: create abstract class InMemoryRepository

---
+++ api/src/shared/domain/repositories/in-memory.repository.ts
@@ -0,0 +1,40 @@
+import { Entity } from '../entities/entity';
+import { NotFoundError } from '../errors/not-found-error';
+import { RepositoryInterface } from './repository-contracts';
+
+export abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = [];
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+
+  async findById(id: string): Promise<E | null> {
+    return this._get(id);
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.items;
+  }
+
+  async update(entity: E): Promise<void> {
+    await this._get(entity.id);
+    const index = this.items.findIndex((item) => item.id === entity.id);
+    this.items[index] = entity;
+  }
+
+  async delete(id: string): Promise<void> {
+    await this._get(id);
+    const index = this.items.findIndex((item) => item.id === id);
+    this.items.splice(index, 1);
+  }
+
+  protected async _get(id: string): Promise<E> {
+    const _id = `${id}`;
+    const entity = this.items.find((item) => item.id === _id);
+    if (!entity) throw new NotFoundError('Entity not found');
+    return entity;
+  }
+}
