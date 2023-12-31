ff4cbba007d5fb59ab2b3cc1ae79b41c8d026e2a
m4chado <enzo653fm@gmail.com>
2023-12-31 16:39:02 -0300

    refactor: improve class PrismaService

--- api/src/shared/infra/database/prisma/prisma.service.ts
+++ api/src/shared/infra/database/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
