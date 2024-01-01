e10ce1f4beefbe6fb0d109ef6d15858bdb641c0e
m4chado <enzo653fm@gmail.com>
2024-01-01 02:54:24 -0300

    feat: implement method findById in UserPrismaRepository

--- api/src/users/infra/database/prisma/repositories/user-prisma.repository.ts
+++ api/src/users/infra/database/prisma/repositories/user-prisma.repository.ts
@@ -1,6 +1,8 @@
 import { PrismaService } from '@/shared/infra/database/prisma/prisma.service';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { UserModelMapper } from '../models/user-model.mapper';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
 
 export class UserPrismaRepository implements UserRepository {
   constructor(private prisma: PrismaService) {}
@@ -14,8 +16,8 @@ export class UserPrismaRepository implements UserRepository {
   insert(entity: UserEntity): Promise<void> {
     throw new Error('Method not implemented.');
   }
-  findById(id: string): Promise<UserEntity> {
-    throw new Error('Method not implemented.');
+  async findById(id: string): Promise<UserEntity> {
+    return this._get(id);
   }
   findAll(): Promise<UserEntity[]> {
     throw new Error('Method not implemented.');
@@ -25,5 +27,16 @@ export class UserPrismaRepository implements UserRepository {
   }
   delete(id: string): Promise<void> {
     throw new Error('Method not implemented.');
+  }
+
+  protected async _get(id: string): Promise<UserEntity> {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { id },
+      });
+      return UserModelMapper.toEntity(user);
+    } catch (error) {
+      throw new NotFoundError(`UserModel not found using ID: ${id}`);
+    }
   }
 }
