c301c54e39782d7966c5be25b043ed087e38b669
m4chado <enzo653fm@gmail.com>
2024-01-02 16:34:05 -0300

    feat: create SearchParams class

--- api/src/shared/domain/repositories/searchable-repository-contracts.ts
+++ api/src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -1,6 +1,93 @@
 import { Entity } from '../entities/entity';
 import { RepositoryInterface } from './repository-contracts';
 
+export type SortDirection = 'asc' | 'desc';
+
+export type SearchProps<Filter> = {
+  page?: number;
+  perPage?: number;
+  sort?: string | null;
+  sortDir?: SortDirection | null;
+  filter?: Filter | null;
+};
+
+export class SearchParams<Filter = string> {
+  protected _page: number;
+  protected _perPage = 15;
+  protected _sort?: string | null;
+  protected _sortDir?: SortDirection | null;
+  protected _filter?: Filter | null;
+
+  constructor(props: SearchProps<Filter> = {}) {
+    this.page = props.page;
+    this.perPage = props.perPage;
+    this.sort = props.sort;
+    this.sortDir = props.sortDir;
+    this.filter = props.filter;
+  }
+
+  get page() {
+    return this._page;
+  }
+
+  private set page(value: number) {
+    let _page = +value;
+    if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
+      _page = 1;
+    }
+    this._page = _page;
+  }
+
+  get perPage() {
+    return this._perPage;
+  }
+
+  private set perPage(value: number) {
+    let _perPage = value === (true as any) ? this._perPage : +value;
+    if (
+      Number.isNaN(_perPage) ||
+      _perPage <= 0 ||
+      parseInt(_perPage as any) !== _perPage
+    ) {
+      _perPage = this._perPage;
+    }
+    this._perPage = _perPage;
+  }
+
+  get sort() {
+    return this._sort;
+  }
+
+  private set sort(value: string | null) {
+    this._sort =
+      value === null || value === undefined || value === '' ? null : `${value}`;
+  }
+
+  get sortDir() {
+    return this._sortDir;
+  }
+
+  private set sortDir(value: SortDirection | null) {
+    if (!this.sort) {
+      this._sortDir = null;
+      return;
+    }
+    const dir = `${value}`.toLowerCase();
+    this._sortDir = dir !== 'asc' && dir !== 'desc' ? 'desc' : dir;
+  }
+
+  get filter(): Filter | null {
+    return this._filter;
+  }
+
+  private set filter(value: Filter | null) {
+    this._filter =
+      value === null || value === undefined || value === ''
+        ? null
+        : (`${value}` as any);
+  }
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
   SearchInput,
