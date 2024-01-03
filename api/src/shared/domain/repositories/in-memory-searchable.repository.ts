6fcb7ea5a2743e33429043a88cc418a046ee15e8
m4chado <enzo653fm@gmail.com>
2024-01-03 17:50:32 -0300

    feat: add search method in InMemorySearchableRepository class

--- api/src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ api/src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -1,12 +1,56 @@
+import { filter } from 'rxjs';
 import { Entity } from '../entities/entity';
 import { InMemoryRepository } from './in-memory.repository';
-import { SearchableRepositoryInterface } from './searchable-repository-contracts';
+import {
+  SearchParams,
+  SearchResult,
+  SearchableRepositoryInterface,
+  SortDirection,
+} from './searchable-repository-contracts';
 
 export abstract class InMemorySearchableRepository<E extends Entity>
   extends InMemoryRepository<E>
   implements SearchableRepositoryInterface<E, any, any>
 {
-  search(props: any): Promise<any> {
-    throw new Error('Method not implemented.');
+  sortableFields: string[];
+  async search(props: SearchParams): Promise<SearchResult<E>> {
+    const itemsFiltered = await this.applyFilter(this.items, props.filter);
+    const itemsSorted = await this.applySort(
+      itemsFiltered,
+      props.sort,
+      props.sortDir,
+    );
+    const itemsPaginated = await this.applyPaginate(
+      itemsSorted,
+      props.page,
+      props.perPage,
+    );
+
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsFiltered.length,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: props.sort,
+      sortDir: props.sortDir,
+      filter: props.filter,
+    });
   }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null,
+  ): Promise<E[]>;
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+  ): Promise<E[]> {}
+
+  protected async applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage'],
+  ): Promise<E[]> {}
 }
--- api/src/shared/domain/repositories/searchable-repository-contracts.ts
+++ api/src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -11,6 +11,16 @@ export type SearchProps<Filter> = {
   filter?: Filter | null;
 };
 
+export type SearchResultProps<E extends Entity, Filter> = {
+  items: E[];
+  total: number;
+  currentPage: number;
+  perPage: number;
+  sort: string | null;
+  sortDir: string | null;
+  filter: Filter | null;
+};
+
 export class SearchParams<Filter = string> {
   protected _page: number;
   protected _perPage = 15;
@@ -88,10 +98,48 @@ export class SearchParams<Filter = string> {
   }
 }
 
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly currentPage: number;
+  readonly perPage: number;
+  readonly lastPage: number;
+  readonly sort: string | null;
+  readonly sortDir: string | null;
+  readonly filter: Filter | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.currentPage = props.currentPage;
+    this.perPage = props.perPage;
+    this.lastPage = Math.ceil(this.total / this.perPage);
+    this.sort = props.sort ?? null;
+    this.sortDir = props.sortDir ?? null;
+    this.filter = props.filter ?? null;
+  }
+
+  toJSON(forceEntity = false) {
+    return {
+      items: forceEntity ? this.items.map((item) => item.toJSON()) : this.items,
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lastPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter,
+    };
+  }
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchInput,
-  SearchOutput,
+  Filter = string,
+  SearchInput = SearchParams<Filter>,
+  SearchOutput = SearchResult<E, Filter>,
 > extends RepositoryInterface<E> {
+  sortableFields: string[];
+
   search(props: SearchInput): Promise<SearchOutput>;
 }
