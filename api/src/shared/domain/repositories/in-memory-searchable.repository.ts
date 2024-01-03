a40bb9da1a867547eaa2d76506671b9e254b11b7
m4chado <enzo653fm@gmail.com>
2024-01-03 17:57:51 -0300

    feat: add applySort method in InMemorySearchableRepository class

--- api/src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ api/src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -12,7 +12,8 @@ export abstract class InMemorySearchableRepository<E extends Entity>
   extends InMemoryRepository<E>
   implements SearchableRepositoryInterface<E, any, any>
 {
-  sortableFields: string[];
+  sortableFields: string[] = [];
+
   async search(props: SearchParams): Promise<SearchResult<E>> {
     const itemsFiltered = await this.applyFilter(this.items, props.filter);
     const itemsSorted = await this.applySort(
@@ -46,7 +47,20 @@ export abstract class InMemorySearchableRepository<E extends Entity>
     items: E[],
     sort: string | null,
     sortDir: SortDirection | null,
-  ): Promise<E[]> {}
+  ): Promise<E[]> {
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return items;
+    }
+    return [...items].sort((a, b) => {
+      if (a.props[sort] < b.props[sort]) {
+        return sortDir === 'asc' ? -1 : 1;
+      }
+      if (a.props[sort] > b.props[sort]) {
+        return sortDir === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+  }
 
   protected async applyPaginate(
     items: E[],
