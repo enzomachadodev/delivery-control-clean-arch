2c60fbad5f949f150065b7261cb1f20b689a2fb4
m4chado <enzo653fm@gmail.com>
2024-01-06 23:11:01 -0300

    refactor: add pagination and filter in ListUserOrdersUseCase

--- src/orders/app/usecases/list-user-orders.usecase.ts
+++ src/orders/app/usecases/list-user-orders.usecase.ts
@@ -1,18 +1,35 @@
 import { UseCase as DefaultUseCase } from '@/shared/app/usecases/usecase';
-import { OrderOutput } from '../dtos/order-output';
 import { OrderRepository } from '@/orders/domain/repositories/order.repository';
+import { SearchInput } from '@/shared/app/dtos/search-input';
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/app/dtos/pagination-output';
+import { OrderOutput } from '../dtos/order-output';
 
 export namespace ListUserOrdersUseCase {
   export type Input = {
     userId: string;
+    search: SearchInput;
   };
 
-  export type Output = OrderOutput[];
+  export type Output = PaginationOutput<OrderOutput>;
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private orderRepository: OrderRepository.Repository) {}
-    execute(input: Input): Promise<Output> {
-      return this.orderRepository.findByUserId(input.userId);
+    async execute(input: Input): Promise<Output> {
+      const { userId, search } = input;
+      const params = new OrderRepository.SearchParams(search);
+      const searchResult = await this.orderRepository.findByUserId(
+        userId,
+        params,
+      );
+      return this.toOutput(searchResult);
+    }
+
+    private toOutput(searchResult: OrderRepository.SearchResult): Output {
+      const { items } = searchResult;
+      return PaginationOutputMapper.toOutput(items, searchResult);
     }
   }
 }
