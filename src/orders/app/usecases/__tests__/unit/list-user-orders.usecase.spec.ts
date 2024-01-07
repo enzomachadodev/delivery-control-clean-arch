99a06371500098d30aa6ddc3e366941d9acc64c2
m4chado <enzo653fm@gmail.com>
2024-01-07 01:17:15 -0300

    test: add toOutput method in ListUserOrdersUseCase unit tests

--- src/orders/app/usecases/__tests__/unit/list-user-orders.usecase.spec.ts
+++ src/orders/app/usecases/__tests__/unit/list-user-orders.usecase.spec.ts
@@ -2,6 +2,8 @@ import { OrderInMemoryRepository } from '@/orders/infra/database/in-memory/repos
 import { ListUserOrdersUseCase } from '../../list-user-orders.usecase';
 import { OrderEntity } from '@/orders/domain/entities/order.entity';
 import { OrderDataBuilder } from '@/orders/domain/testing/helpers/order-data-builder';
+import { OrderRepository } from '@/orders/domain/repositories/order.repository';
+import { randomUUID } from 'crypto';
 
 describe('ListUserOrdersUseCase unit tests', () => {
   let sut: ListUserOrdersUseCase.UseCase;
@@ -12,15 +14,131 @@ describe('ListUserOrdersUseCase unit tests', () => {
     sut = new ListUserOrdersUseCase.UseCase(orderRepository);
   });
 
-  it('should return the orders of a user', async () => {
-    const order1 = new OrderEntity(OrderDataBuilder({}));
-    const order2 = new OrderEntity(OrderDataBuilder({ userId: order1.userId }));
+  it('toOutput method', () => {
+    let result = new OrderRepository.SearchResult({
+      items: [],
+      total: 1,
+      currentPage: 1,
+      perPage: 2,
+      sort: null,
+      sortDir: null,
+      filter: null,
+    });
+    let output = sut['toOutput'](result);
+    expect(output).toStrictEqual({
+      items: [],
+      total: 1,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 2,
+    });
 
-    const items = [order1, order2];
+    const entity = new OrderEntity(OrderDataBuilder({}));
+    result = new OrderRepository.SearchResult({
+      items: [entity],
+      total: 1,
+      currentPage: 1,
+      perPage: 2,
+      sort: null,
+      sortDir: null,
+      filter: null,
+    });
+    output = sut['toOutput'](result);
+    expect(output).toStrictEqual({
+      items: [entity.toJSON()],
+      total: 1,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 2,
+    });
+  });
+
+  it('should return the orders of a user ordered by createdAt', async () => {
+    const createdAt = new Date();
+    const userId = randomUUID();
+
+    const items = [
+      new OrderEntity(OrderDataBuilder({ userId, createdAt })),
+      new OrderEntity(
+        OrderDataBuilder({
+          userId,
+          createdAt: new Date(createdAt.getTime() + 1),
+        }),
+      ),
+    ];
     orderRepository.items = items;
-    const output = await sut.execute({ userId: order1.userId });
-    expect(output).toHaveLength(2);
-    expect(output[0]).toStrictEqual(order1);
-    expect(output[1]).toStrictEqual(order2);
+    const output = await sut.execute({ userId, search: {} });
+    expect(output).toStrictEqual({
+      items: [...items].reverse().map((item) => item.toJSON()),
+      total: 2,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 15,
+    });
+  });
+
+  it('should return the orders of a user using pagination, sort and filter', async () => {
+    const userId = randomUUID();
+    const items = [
+      new OrderEntity(OrderDataBuilder({ userId, customerName: 'a' })),
+      new OrderEntity(OrderDataBuilder({ userId, customerName: 'AA' })),
+      new OrderEntity(OrderDataBuilder({ userId, customerName: 'Aa' })),
+      new OrderEntity(OrderDataBuilder({ userId, customerName: 'b' })),
+      new OrderEntity(OrderDataBuilder({ userId, customerName: 'c' })),
+    ];
+    orderRepository.items = items;
+    let output = await sut.execute({
+      userId,
+      search: {
+        page: 1,
+        perPage: 2,
+        sort: 'customerName',
+        sortDir: 'asc',
+        filter: 'a',
+      },
+    });
+    expect(output).toStrictEqual({
+      items: [items[1].toJSON(), items[2].toJSON()],
+      total: 3,
+      currentPage: 1,
+      lastPage: 2,
+      perPage: 2,
+    });
+
+    output = await sut.execute({
+      userId,
+      search: {
+        page: 2,
+        perPage: 2,
+        sort: 'customerName',
+        sortDir: 'asc',
+        filter: 'a',
+      },
+    });
+    expect(output).toStrictEqual({
+      items: [items[0].toJSON()],
+      total: 3,
+      currentPage: 2,
+      lastPage: 2,
+      perPage: 2,
+    });
+
+    output = await sut.execute({
+      userId,
+      search: {
+        page: 1,
+        perPage: 3,
+        sort: 'customerName',
+        sortDir: 'desc',
+        filter: 'a',
+      },
+    });
+    expect(output).toStrictEqual({
+      items: [items[0].toJSON(), items[2].toJSON(), items[1].toJSON()],
+      total: 3,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 3,
+    });
   });
 });
