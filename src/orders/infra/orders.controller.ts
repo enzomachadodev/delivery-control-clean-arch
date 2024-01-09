021ff169a2cf6589883ef5c730de86d144cf2a62
m4chado <enzo653fm@gmail.com>
2024-01-09 14:46:19 -0300

    feat: implemente order usecases in OrdersController

--- src/orders/infra/orders.controller.ts
+++ src/orders/infra/orders.controller.ts
@@ -6,37 +6,72 @@ import {
   Patch,
   Param,
   Delete,
+  Inject,
+  Query,
 } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { CreateOrderDto } from './dtos/create-order.dto';
-import { UpdateOrderDto } from './dtos/update-order.dto';
+import { CreateOrderUseCase } from '../app/usecases/create-order.usecase';
+import { GetOrderUseCase } from '../app/usecases/get-order.usecase';
+import { UpdateOrderStatusUseCase } from '../app/usecases/update-order-status.usecase';
+import { ListUserOrdersUseCase } from '../app/usecases/list-user-orders.usecase';
+import { DeleteOrderUseCase } from '../app/usecases/delete-order.usecase';
+import {
+  CreateOrderDto,
+  ListUserOrdersDto,
+  UpdateOrderStatusDto,
+} from './dtos';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+  @Inject(CreateOrderUseCase.UseCase)
+  private createOrderUseCase: CreateOrderUseCase.UseCase;
+
+  @Inject(GetOrderUseCase.UseCase)
+  private getOrderUseCase: GetOrderUseCase.UseCase;
+
+  @Inject(UpdateOrderStatusUseCase.UseCase)
+  private updateOrderStatusUseCase: UpdateOrderStatusUseCase.UseCase;
+
+  @Inject(ListUserOrdersUseCase.UseCase)
+  private listUserOrdersUseCase: ListUserOrdersUseCase.UseCase;
+
+  @Inject(DeleteOrderUseCase.UseCase)
+  private deleteOrderUseCase: DeleteOrderUseCase.UseCase;
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+    const userId = 'string';
+    console.log(this.createOrderUseCase, '@@@@@@@@@@@@2');
+    return this.createOrderUseCase.execute({ ...createOrderDto, userId });
   }
 
   @Get()
-  findAll() {
-    return this.ordersService.findAll();
+  findByUser(@Query() searchParams: ListUserOrdersDto) {
+    const userId = 'string';
+    return this.listUserOrdersUseCase.execute({ userId, ...searchParams });
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(+id);
+    const userId = 'string';
+    return this.getOrderUseCase.execute({ id, userId });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(+id, updateOrderDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateOrderDto: UpdateOrderStatusDto,
+  ) {
+    const userId = 'string';
+    return this.updateOrderStatusUseCase.execute({
+      userId,
+      orderId: id,
+      status: updateOrderDto.status,
+    });
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.ordersService.remove(+id);
+    const userId = 'string';
+    return this.deleteOrderUseCase.execute({ orderId: id, userId });
   }
 }
--- src/orders/infra/orders.module.ts
+++ src/orders/infra/orders.module.ts
@@ -1,9 +1,57 @@
 import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { PrismaService } from '@/shared/infra/database/prisma/prisma.service';
+import { OrderPrismaRepository } from './database/prisma/repositories/order-prisma.repository';
+import { CreateOrderUseCase } from '../app/usecases/create-order.usecase';
+import { UserPrismaRepository } from '@/users/infra/database/prisma/repositories/user-prisma.repository';
+import { StatusHistoryPrismaRepository } from '@/status-history/infra/database/prisma/repositories/status-history-prisma.repository';
+import { StatusHistoryRepository } from '@/status-history/domain/repositories/status-history.repository';
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { OrderRepository } from '../domain/repositories/order.repository';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [
+    {
+      provide: 'PrismaService',
+      useClass: PrismaService,
+    },
+    {
+      provide: 'OrderRepository',
+      useFactory(prismaService: PrismaService) {
+        return new OrderPrismaRepository(prismaService);
+      },
+      inject: ['PrismaService'],
+    },
+    {
+      provide: 'UserRepository',
+      useFactory(prismaService: PrismaService) {
+        return new UserPrismaRepository(prismaService);
+      },
+      inject: ['PrismaService'],
+    },
+    {
+      provide: 'StatusHistoryRepository',
+      useFactory(prismaService: PrismaService) {
+        return new StatusHistoryPrismaRepository(prismaService);
+      },
+      inject: ['PrismaService'],
+    },
+    {
+      provide: CreateOrderUseCase.UseCase,
+      useFactory(
+        orderRepository: OrderRepository.Repository,
+        userRepository: UserRepository,
+        statusHistoryRepository: StatusHistoryRepository,
+      ) {
+        return new CreateOrderUseCase.UseCase(
+          orderRepository,
+          userRepository,
+          statusHistoryRepository,
+        );
+      },
+      inject: ['OrderRepository', 'UserRepository', 'StatusHistoryRepository'],
+    },
+  ],
 })
 export class OrdersModule {}
