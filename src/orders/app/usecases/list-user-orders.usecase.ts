dedbd691ee7d7b5b50529b61d7eb94aa28c0ca1e
m4chado <enzo653fm@gmail.com>
2024-01-09 14:44:18 -0300

    refactor: change ListUserOrdersUseCase input

--- src/orders/app/usecases/list-user-orders.usecase.ts
+++ src/orders/app/usecases/list-user-orders.usecase.ts
@@ -8,9 +8,8 @@ import {
 import { OrderOutput, OrderOutputMapper } from '../dtos/order-output';
 
 export namespace ListUserOrdersUseCase {
-  export type Input = {
+  export type Input = SearchInput & {
     userId: string;
-    search: SearchInput;
   };
 
   export type Output = PaginationOutput<OrderOutput>;
@@ -18,7 +17,7 @@ export namespace ListUserOrdersUseCase {
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private orderRepository: OrderRepository.Repository) {}
     async execute(input: Input): Promise<Output> {
-      const { userId, search } = input;
+      const { userId, ...search } = input;
       const params = new OrderRepository.SearchParams(search);
       const searchResult = await this.orderRepository.findByUserId(
         userId,
