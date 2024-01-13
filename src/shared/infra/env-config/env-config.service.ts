8e33e2b9a31f89d6722ca498715577fa1d137f4b
m4chado <enzo653fm@gmail.com>
2024-01-13 19:11:49 -0300

    refactor: include jwt features in EnvConfigProvider

--- src/shared/infra/env-config/env-config.service.ts
+++ src/shared/infra/env-config/env-config.service.ts
@@ -5,12 +5,19 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class EnvConfigService implements EnvConfig {
   constructor(private configService: ConfigService) {}
-
   getAppPort(): number {
     return Number(this.configService.get('PORT'));
   }
 
   getNodeEnv(): string {
     return this.configService.get('NODE_ENV');
+  }
+
+  getJwtSecret(): string {
+    return this.configService.get('JWT_SECRET');
+  }
+
+  getJwtExpiresInSeconds(): number {
+    return Number(this.configService.get('JWT_EXPIRES_IN'));
   }
 }
