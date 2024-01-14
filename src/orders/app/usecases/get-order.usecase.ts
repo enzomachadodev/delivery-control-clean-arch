f69facb6a951822459e4938c002a6f58f2c959f7
m4chado <enzo653fm@gmail.com>
2024-01-14 13:50:56 -0300

    refactor: remove status history from response

--- src/orders/app/usecases/get-order.usecase.ts
+++ src/orders/app/usecases/get-order.usecase.ts
@@ -1,11 +1,6 @@
 import { UseCase as DefaultUseCase } from '@/shared/app/usecases/usecase';
 import { OrderOutput, OrderOutputMapper } from '../dtos/order-output';
 import { OrderRepository } from '@/orders/domain/repositories/order.repository';
-import {
-  StatusHistoryOutput,
-  StatusHistoryOutputMapper,
-} from '@/status-history/app/dtos/status-history-output';
-import { StatusHistoryRepository } from '@/status-history/domain/repositories/status-history.repository';
 import { UnauthorizedError } from '@/shared/app/errors/unauthorized-error';
 import { BadRequestError } from '@/shared/app/errors/bad-request-error';
 
@@ -15,31 +10,20 @@ export namespace GetOrderUseCase {
     userId: string;
   };
 
-  export type Output = OrderOutput & {
-    statusHistory: StatusHistoryOutput[];
-  };
+  export type Output = OrderOutput;
 
   export class UseCase implements DefaultUseCase<Input, Output> {
-    constructor(
-      private orderRepository: OrderRepository.Repository,
-      private statusHistoryRepository: StatusHistoryRepository,
-    ) {}
+    constructor(private orderRepository: OrderRepository.Repository) {}
 
     async execute(input: Input): Promise<Output> {
-      if (!input.userId) throw new BadRequestError('Input data not provided');
-      const orderEntity = await this.orderRepository.findById(input.id);
+      const { userId, id } = input;
+      if (!userId) throw new BadRequestError('Input data not provided');
+      const orderEntity = await this.orderRepository.findById(id);
 
-      if (orderEntity.userId !== input.userId)
+      if (orderEntity.userId !== userId)
         throw new UnauthorizedError('Insufficient permission');
-      const statusHistoryEntities =
-        await this.statusHistoryRepository.findByOrderId(input.id);
 
-      const order = OrderOutputMapper.toOutput(orderEntity);
-      console.log(order);
-      const statusHistory = statusHistoryEntities.map(
-        StatusHistoryOutputMapper.toOutput,
-      );
-      return { ...order, statusHistory };
+      return OrderOutputMapper.toOutput(orderEntity);
     }
   }
 }
