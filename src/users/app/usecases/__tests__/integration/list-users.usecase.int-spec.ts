fe0c0a9dff89c0fae0d5b6c51f80b48cbe569355
m4chado <enzo653fm@gmail.com>
2024-01-18 10:48:03 -0300

    test: create ListUsersUseCase integration test

---
+++ src/users/app/usecases/__tests__/integration/list-users.usecase.int-spec.ts
@@ -0,0 +1,44 @@
+import { DatabaseModule } from '@/shared/infra/database/database.module';
+import { setupPrismaTests } from '@/shared/infra/database/prisma/testing/setup-prisma-tests';
+import { UserPrismaRepository } from '@/users/infra/database/prisma/repositories/user-prisma.repository';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { ListUsersUseCase } from '../../list-users.usecase';
+
+describe('ListUsersUseCase integration tests', () => {
+  const prismaService = new PrismaClient();
+  let sut: ListUsersUseCase.UseCase;
+  let repository: UserPrismaRepository;
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    setupPrismaTests();
+    module = await Test.createTestingModule({
+      imports: [DatabaseModule.forTest(prismaService)],
+    }).compile();
+    repository = new UserPrismaRepository(prismaService as any);
+  });
+
+  beforeEach(async () => {
+    sut = new ListUsersUseCase.UseCase(repository);
+    await prismaService.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
+  it('should returns all users', async () => {
+    const entity = new UserEntity(UserDataBuilder({}));
+    const model = await prismaService.user.create({
+      data: entity.toJSON(),
+    });
+    console.log(model);
+
+    const output = await sut.execute();
+
+    expect(output).toStrictEqual([model]);
+  });
+});
