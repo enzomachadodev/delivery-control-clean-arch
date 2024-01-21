8d093ff9f32e69bb0216a13d64497c589371a45e
m4chado <enzo653fm@gmail.com>
2024-01-21 11:27:42 -0300

    docs: add Swagger decorators on presenters

--- src/orders/infra/presenters/order.presenter.ts
+++ src/orders/infra/presenters/order.presenter.ts
@@ -1,25 +1,48 @@
 import { OrderOutput } from '@/orders/app/dtos/order-output';
 import { OrderStatus } from '@/status-history/domain/entities/status-history.entity';
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 
 export class OrderPresenter {
+  @ApiProperty({ description: 'Order identification' })
   id: string;
+
+  @ApiProperty({ description: 'Identification of the user creating the order' })
   userId: string;
+
+  @ApiProperty({ description: 'Customer name' })
   customerName: string;
+
+  @ApiProperty({ description: 'Customer street' })
   street: string;
+
+  @ApiProperty({ description: 'Customer number' })
   number: number;
+
+  @ApiProperty({ description: 'Customer complement' })
   complement: string | null;
+
+  @ApiProperty({ description: 'Customer neighborhood' })
   neighborhood: string;
+
+  @ApiProperty({ description: 'Customer city' })
   city: string;
+
+  @ApiProperty({ description: 'Customer state' })
   state: string;
+
+  @ApiProperty({ description: 'Customer zip code' })
   zipCode: string;
 
+  @ApiProperty({ description: 'Current order status ' })
   @Transform(({ value }: { value: OrderStatus }) => OrderStatus[value])
   currentStatus: OrderStatus;
 
+  @ApiProperty({ description: 'Order creation date' })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   createdAt: Date;
 
+  @ApiProperty({ description: 'last order update' })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   updatedAt: Date;
 
--- src/users/infra/presenters/user.presenter.ts
+++ src/users/infra/presenters/user.presenter.ts
@@ -1,12 +1,22 @@
 import { UserOutput } from '@/users/app/dtos/user-output';
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 
 export class UserPresenter {
+  @ApiProperty({ description: 'User identification' })
   id: string;
+
+  @ApiProperty({ description: 'Username' })
   name: string;
+
+  @ApiProperty({ description: 'User email' })
   email: string;
+
+  @ApiProperty({ description: 'User creation date' })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   createdAt: Date;
+
+  @ApiProperty({ description: 'Last user update' })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   updatedAt: Date;
 
