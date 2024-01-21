23777d5fb80a7b931124fb8a9956dae090189e55
m4chado <enzo653fm@gmail.com>
2024-01-21 01:39:04 -0300

    refactor: improve UpdateOrderStatusDto validators

--- src/orders/infra/dtos/update-order-status.dto.ts
+++ src/orders/infra/dtos/update-order-status.dto.ts
@@ -1,11 +1,13 @@
 import { UpdateOrderStatusUseCase } from '@/orders/app/usecases/update-order-status.usecase';
+import { IsOrderStatusHistoryValidator } from '@/orders/domain/validators/order.validator';
 import { OrderStatus } from '@/status-history/domain/entities/status-history.entity';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Validate } from 'class-validator';
 
 export class UpdateOrderStatusDto
   implements Omit<UpdateOrderStatusUseCase.Input, 'orderId' | 'userId'>
 {
   @IsString()
   @IsNotEmpty()
+  @Validate(IsOrderStatusHistoryValidator)
   status: OrderStatus;
 }
